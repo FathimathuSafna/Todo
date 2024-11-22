f4849b508e485ce88136c06f9c1d3434b04fd3d0
safna <fathimathu29@gmail.com>
2024-11-22 23:43:44 +0530

    updation in deleteProjeects

--- controllers/TodoControllers.js
+++ controllers/TodoControllers.js
@@ -1,67 +1,87 @@
 import Project from "../models/projectSchema.js";
 import Todo from "../models/TodoSchema.js";
 
-const addTodo = async (req,res)=>{  
-    const { projectId } = req.body
-    try{
-        const isProjectExist = await Project.findById(projectId)
-        console.log(isProjectExist)
-        if(isProjectExist){
-        const newTodo = await Todo.create(req.body)
-        isProjectExist.list_of_Todos.push(newTodo._id) // append the todo to the listOfTodo to the project db
-        await isProjectExist.save()
-        res.status(201).json({ 
-            msg:"Details added succesfully",
-            data:newTodo
-        })
+const addTodo = async (req, res) => {
+  const { projectId } = req.body;
+  try {
+    const isProjectExist = await Project.findById(projectId);
+    console.log(isProjectExist);
+    if (isProjectExist) {
+      const newTodo = await Todo.create(req.body);
+      isProjectExist.list_of_Todos.push(newTodo._id); // append the todo to the listOfTodo to the project db
+      await isProjectExist.save();
+      res.status(201).json({
+        msg: "Details added succesfully",
+        data: newTodo,
+      });
     } else {
-        res.status(400).json({
-            msg :"invalid id"
-        })
+      res.status(400).json({
+        msg: "invalid id",
+      });
     }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
+const getTodo = async (req, res) => {
+  try {
+    let id = req.params.id;
+    if (!id) {
+      return res.status(400).json({
+        msg: "Invalid id",
+      });
     }
-    catch(err){
-        res.status(400).json(err)
-    }
-}
-const updateTodo = async(req,res)=>{
-    const { description,status } = req.body
-    try{
-        let id = req.params.id
-            const updateTodo = await Todo.updateOne({_id:id},{$set:{status,description}})
-        res.status(201).json({
-            msg:"details updated succesfully",
-            data:updateTodo
-           
-        })
-    } catch (err) {
-        res.status(400).json(err)
-    }
-}
+    const ProjectDetails = await Project.findById(id).populate("list_of_Todos");
+    res.status(201).json({
+      msg: "suceesfully retrived",
+      data: ProjectDetails,
+    });
+  } catch (err) {
+    console.log(err);
 
-const deleteTodo = async (req, res) => {
-    const toDoId = req.params.id; 
-    try {
-        const toDo = await Todo.findById(toDoId);
-        if (!toDo) {
-            return res.status(404).json({ msg: "Todo not found" });
-        }
-        const updatedProjects = await Project.updateMany(
-            { list_of_Todos: { $in: [toDo._id] } }, // Find Projects that reference this Todo
-            { $pull: { list_of_Todos: toDo._id } }   // pull for Remove the Todo ID from the list_of_Todos array
-        )
-        const deletedTodo = await Todo.findByIdAndDelete(toDoId);
-        res.status(200).json({
-            msg: "Todo and its references in Projects deleted successfully",
-            data: {
-                updatedProjects: updatedProjects.modifiedCount, // Count of projects where the Todo was removed
-                deletedTodo
-            }
-        });
-    } catch (err) {
-        res.status(400).json({ msg: "Error occurred", error: err.message });
-    }
+    res.status(400).json(err);
+  }
 };
 
+const updateTodo = async (req, res) => {
+  const { description, status } = req.body;
+  try {
+    let id = req.params.id;
+    const updateTodo = await Todo.updateOne(
+      { _id: id },
+      { $set: { status, description } }
+    );
+    res.status(201).json({
+      msg: "details updated succesfully",
+      data: updateTodo,
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+};
 
-export {addTodo,updateTodo,deleteTodo}
+const deleteTodo = async (req, res) => {
+  const toDoId = req.params.id;
+  try {
+    const toDo = await Todo.findById(toDoId);
+    if (!toDo) {
+      return res.status(404).json({ msg: "Todo not found" });
+    }
+    const updatedProjects = await Project.updateMany(
+      { list_of_Todos: { $in: [toDo._id] } }, // Find Projects that reference this Todo
+      { $pull: { list_of_Todos: toDo._id } } // pull for Remove the Todo ID from the list_of_Todos array
+    );
+    const deletedTodo = await Todo.findByIdAndDelete(toDoId);
+    res.status(200).json({
+      msg: "Todo and its references in Projects deleted successfully",
+      data: {
+        updatedProjects: updatedProjects.modifiedCount, // Count of projects where the Todo was removed
+        deletedTodo,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({ msg: "Error occurred", error: err.message });
+  }
+};
+
+export { addTodo, getTodo, updateTodo, deleteTodo };
--- routes/TodoRoutes.js
+++ routes/TodoRoutes.js
@@ -1,9 +1,9 @@
 import express from 'express'
-import { addTodo,updateTodo,deleteTodo } from '../controllers/TodoControllers.js'
+import { addTodo,getTodo,updateTodo,deleteTodo } from '../controllers/TodoControllers.js'
 
 const app = express.Router()
 
 app.route('/').post(addTodo)
-app.route('/:id').put(updateTodo).delete(deleteTodo)
+app.route('/:id').put(updateTodo).delete(deleteTodo).get(getTodo)
 
 export default app
