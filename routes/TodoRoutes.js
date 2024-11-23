994780b697bbf192db92623174d56b232ad4e5c9
safna <fathimathu29@gmail.com>
2024-11-23 18:16:36 +0530

    add jwt

--- routes/TodoRoutes.js
+++ routes/TodoRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import { addTodo,getTodo,updateTodo,deleteTodo } from '../controllers/TodoControllers.js'
+import protect from '../middleWare/userMiddleware.js'
+
 
 const app = express.Router()
 
-app.route('/').post(addTodo)
-app.route('/:id').put(updateTodo).delete(deleteTodo).get(getTodo)
+app.route('/').post(protect,addTodo)
+app.route('/:id').put(protect,updateTodo).delete(protect,deleteTodo).get(protect,getTodo)
 
 export default app
